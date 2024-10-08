bd59b515961aa3d37b43cbcc8339eedd9ba362e3
MnqobiMhlongo <nqobimhlongo86@gmail.com>
2024-10-08 19:31:00 +0200

    mail changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -32,5 +33,46 @@ namespace lifestyleEstate.Controllers
         {
             return View();
         }
+
+
+
+        [HttpPost]
+        public ActionResult SendEmail(string name, string email, string subject, string message)
+        {
+            try
+            {
+                // Set up the email
+                MailMessage mail = new MailMessage();
+                mail.To.Add("engomesite@gmail.com"); // Destination email
+                mail.From = new MailAddress(email); // Sender's email
+                mail.Subject = $"General Inquiry From {name}"; // Dynamic subject
+                mail.Body = $"<p>{message}</p>"; // Message from form
+                mail.IsBodyHtml = true; // Enable HTML content
+
+                // Configure SMTP client
+                SmtpClient smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    UseDefaultCredentials = false,
+                    Credentials = new System.Net.NetworkCredential("engomesite@gmail.com", "mhiv ucld hxnj gred"), // Secure this part
+                    EnableSsl = true
+                };
+
+                // Send the email
+                smtp.Send(mail);
+
+                // Redirect to success or homepage after sending
+                return RedirectToAction("Home", "Home", new { successMessage = "Email Sent, Agent Will Be In Contact Soon" });
+
+            }
+            catch (Exception ex)
+            {
+                // Handle exception (log it, show a message, etc.)
+                return RedirectToAction("Home", "Home", new { successMessage = "Error Sending Email, Please check email or try again later" });
+
+            }
+        }
+
     }
 }
